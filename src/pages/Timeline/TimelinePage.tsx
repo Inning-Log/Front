f988392dd9c0130991ec3f4e30a0f9e49c3effcf
heexion <alsgml030901@naver.com>
2026-07-08 21:20:21 +0900

    FEAT: 타임라인 페이지에 공통 헤더 적용

--- src/pages/Timeline/TimelinePage.tsx
+++ src/pages/Timeline/TimelinePage.tsx
@@ -1,3 +1,13 @@
+import { PageHeader } from "../../app/layouts/PageHeader";
+
 export function TimelinePage() {
-  return <div>TimelinePage</div>;
+  return (
+    <div className="min-h-dvh w-full">
+      <PageHeader title="타임라인" />
+
+      <main>
+        TimelinePage
+      </main>
+    </div>
+  );
 }
