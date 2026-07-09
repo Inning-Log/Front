d87fb6365d71cbefa89aefaa3b0e1cd4e0393240
wooooyun <wooyunjay@gmail.com>
2026-07-09 18:34:11 +0900

    refactor: 헤더를 공통으로 사용 가능하도록 수정

--- src/pages/Home/NotificationPage.tsx
+++ src/pages/Home/NotificationPage.tsx
@@ -3,11 +3,13 @@ import { PageHeader } from "../../app/layouts/PageHeader";
 export function NotificationPage() {
   return (
     <div className="min-h-dvh w-full">
-      <PageHeader title="알림" />
+      <PageHeader
+        title="알림"
+        rightText="알림 설정"
+        rightTo="/home/notifications/settings"
+      />
 
-      <main>
-        NotificationPage
-      </main>
+      <main>NotificationPage</main>
     </div>
   );
 }
