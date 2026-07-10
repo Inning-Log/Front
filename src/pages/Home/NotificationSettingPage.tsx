75c658a4ab6d04320a51ff975836b0ad3b5a3764
heexion <alsgml030901@naver.com>
2026-07-11 00:09:10 +0900

    FEAT: 알림 설정 페이지 구현

--- src/pages/Home/NotificationSettingPage.tsx
+++ src/pages/Home/NotificationSettingPage.tsx
@@ -1,3 +1,39 @@
+import { useState } from "react";
+
+import { PageHeader } from "../../app/layouts/PageHeader";
+import { NotificationSettingItem } from "../../features/home/components/NotificationSettingItem";
+
 export function NotificationSettingPage() {
-  return <div>NotificationSettingPage</div>;
+  const [gameNotification, setGameNotification] = useState(false);
+  const [recordNotification, setRecordNotification] = useState(true);
+  const [reactionNotification, setReactionNotification] = useState(true);
+
+  return (
+    <main className="min-h-dvh w-full bg-bg-primary pt-[45px]">
+      <PageHeader title="알림 설정" />
+
+      <section className="mx-auto mt-[25px] w-full max-w-[340px]">
+        <NotificationSettingItem
+          title="경기 진행 상황 알림"
+          description="팀 득실점 및 이닝 시작 종료 알림"
+          isEnabled={gameNotification}
+          onToggle={() => setGameNotification((prev) => !prev)}
+        />
+
+        <NotificationSettingItem
+          title="기록 독촉 알림"
+          description="팀 득실점 및 이닝 시작 종료 알림"
+          isEnabled={recordNotification}
+          onToggle={() => setRecordNotification((prev) => !prev)}
+        />
+
+        <NotificationSettingItem
+          title="댓글 또는 반응"
+          description="타임라인에 남긴 친구 반응"
+          isEnabled={reactionNotification}
+          onToggle={() => setReactionNotification((prev) => !prev)}
+        />
+      </section>
+    </main>
+  );
 }
