edc8263648ac21739a136a817abfe2c1606fee1b
heexion <alsgml030901@naver.com>
2026-07-11 14:36:17 +0900

    FEAT: 알림 페이지에 알림 아이템 추가

--- src/pages/Home/NotificationPage.tsx
+++ src/pages/Home/NotificationPage.tsx
@@ -1,15 +1,35 @@
 import { PageHeader } from "../../app/layouts/PageHeader";
+import { NotificationItem } from "../../features/home/components/NotificationItem";
+
+const notifications = [
+  {
+    id: 1,
+    userId: "inning",
+    userName: "이닝로그",
+  }
+];
 
 export function NotificationPage() {
   return (
-    <div className="min-h-dvh w-full">
+    <div className="min-h-dvh w-full pt-[45px]">
       <PageHeader
         title="알림"
         rightText="알림 설정"
         rightTo="/home/notifications/settings"
       />
 
-      <main>NotificationPage</main>
+      <main className="flex flex-col items-center gap-[12px] px-[16px] pt-[20px]">
+        {notifications.map((notification) => (
+          <NotificationItem
+            key={notification.id}
+            userId={notification.userId}
+            userName={notification.userName}
+            onDelete={() => {
+              console.log(`${notification.userId} 알림 삭제`);
+            }}
+          />
+        ))}
+      </main>
     </div>
   );
 }
