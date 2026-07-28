8a49df6c5e6baa2da09e23c48f789702d8304558
heexion <alsgml030901@naver.com>
2026-07-28 19:33:09 +0900

    FEAT: 일반 알림 컴포넌트 추가

---
+++ src/features/home/components/NotificationItem.tsx
@@ -0,0 +1,28 @@
+import type { ReactNode } from "react";
+
+export type NotificationCategory =
+  | "친구 알림"
+  | "경기 알림"
+  | "기록 알림";
+
+type NotificationItemProps = {
+  category: NotificationCategory;
+  message: ReactNode;
+};
+
+export function NotificationItem({
+  category,
+  message,
+}: NotificationItemProps) {
+  return (
+    <article className="flex h-[66px] w-[calc(100%_-_32px)] max-w-[398px] flex-col justify-center rounded-[37.5px] bg-white px-[36px] shadow-[0_2px_17.5px_-8px_rgba(0,0,0,0.18)]">
+      <p className="text-caption truncate text-text-tertiary">
+        {category}
+      </p>
+
+      <p className="text-label-4 truncate text-black">
+        {message}
+      </p>
+    </article>
+  );
+}
