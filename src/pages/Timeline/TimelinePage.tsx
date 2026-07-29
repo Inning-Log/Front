4b93c6bc5a08a089f32879875cf18a30ebda76ab
heexion <alsgml030901@naver.com>
2026-07-29 21:48:08 +0900

    FEAT: 타임라인 페이지에 경기 정보 섹션 추가

--- src/pages/Timeline/TimelinePage.tsx
+++ src/pages/Timeline/TimelinePage.tsx
@@ -1,5 +1,8 @@
+import doosanBearsMascot from "../../assets/icons/teammascot/doosanbears.svg";
+import lgTwinsMascot from "../../assets/icons/teammascot/lgtwins.svg";
 import { PageHeader } from "../../app/layouts/PageHeader";
 import { InningCard } from "../../features/timeline/components/InningCard";
+import { TimelineGameScore } from "../../features/timeline/components/TimelineGameScore";
 import { TimelineProfileList } from "../../features/timeline/components/TimelineProfileList";
 
 const innings = [1, 2, 3, 4, 5, 6, 7, 8, 9];
@@ -10,6 +13,17 @@ export function TimelinePage() {
       <PageHeader title="타임라인" />
 
       <TimelineProfileList />
+
+      <TimelineGameScore
+        homeTeamName="두산 베어스"
+        homeTeamLogo={doosanBearsMascot}
+        awayTeamName="LG 트윈스"
+        awayTeamLogo={lgTwinsMascot}
+        homeScore={1}
+        awayScore={0}
+        gameDateTime="07.29 18:30"
+        stadium="잠실 야구장"
+      />
 
       <main className="flex flex-col items-center gap-[12px] px-[16px] pt-[20px]">
         {innings.map((inning) => (
