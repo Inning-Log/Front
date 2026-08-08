40ef44e6d1e4d62843e8520a4e5110bdd2f9addf
heexion <alsgml030901@naver.com>
2026-08-08 16:30:05 +0900

    REFACTOR: 내 영상 템플릿 마스코트 처리 방식 변경

--- src/features/timeline/components/TimelineMyVideoTemplate.tsx
+++ src/features/timeline/components/TimelineMyVideoTemplate.tsx
@@ -1,5 +1,3 @@
-import doosanBearsMascot from "../../../assets/icons/teammascot/doosanbears.svg";
-import lgTwinsMascot from "../../../assets/icons/teammascot/lgtwins.svg";
 import type { InningRecord } from "./InningCard";
 import { TimelineGameScore } from "./TimelineGameScore";
 import { TimelineMyVideoInningCard } from "./TimelineMyVideoInningCard";
@@ -21,9 +19,7 @@ export function TimelineMyVideoTemplate({
     <div className="flex h-[844px] w-[390px] flex-col bg-white px-[16px] pt-[40px]">
       <TimelineGameScore
         homeTeamName="두산 베어스"
-        homeTeamLogo={doosanBearsMascot}
         awayTeamName="LG 트윈스"
-        awayTeamLogo={lgTwinsMascot}
         homeScore={1}
         awayScore={0}
         gameDateTime="06.03 18:30"
