0a57857b64efeb83421459db9b478959354bc07c
heexion <alsgml030901@naver.com>
2026-08-08 16:21:18 +0900

    FEAT: 내 영상 저장 템플릿 프리뷰 추가

--- src/pages/Timeline/TimelineVideoPreviewPage.tsx
+++ src/pages/Timeline/TimelineVideoPreviewPage.tsx
@@ -1,19 +1,17 @@
-import {
-  TimelineFriendVideoTemplate,
-} from "../../features/timeline/components/TimelineFriendVideoTemplate";
+//import { TimelineFriendVideoTemplate } from "../../features/timeline/components/TimelineFriendVideoTemplate";
+import { TimelineMyVideoTemplate } from "../../features/timeline/components/TimelineMyVideoTemplate";
 
-
-//기록 없는 경우
-const myRecord = {
-  name: "민희",
-  teamName: "두산",
-};
-
+// 친구랑 함께 저장
 // const myRecord = {
 //   name: "민희",
 //   teamName: "두산",
+// };
+
+// const friendRecord = {
+//   name: "지민",
+//   teamName: "한화",
 //   record: {
-//     id: "my-record-1",
+//     id: "friend-record-1",
 //     inning: 1,
 //     recordedAt: "18:43",
 //     homeScore: 0,
@@ -22,26 +20,32 @@ const myRecord = {
 //   },
 // };
 
-const friendRecord = {
-  name: "지민",
-  teamName: "한화",
-  record: {
-    id: "friend-record-1",
-    inning: 1,
-    recordedAt: "18:43",
-    homeScore: 0,
-    awayScore: 0,
-    text: "야호~~~~",
-  },
+// 내 영상 저장
+const myVideoRecord = {
+  id: "my-record-1",
+  inning: 1,
+  recordedAt: "18:43",
+  homeScore: 1,
+  awayScore: 0,
+  text: "야호~~~~",
 };
 
 export function TimelineVideoPreviewPage() {
   return (
     <div className="flex min-h-dvh w-full justify-center bg-[#2E312F] py-[40px]">
-      <TimelineFriendVideoTemplate
+      {/* 친구랑 함께 저장 */}
+      {/* <TimelineFriendVideoTemplate
         inning={1}
         myRecord={myRecord}
         friendRecord={friendRecord}
+      /> */}
+
+      {/* 내 영상 저장 */}
+      <TimelineMyVideoTemplate
+        inning={1}
+        userName="minhee"
+        teamName="두산"
+        record={myVideoRecord}
       />
     </div>
   );
